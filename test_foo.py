9a8bffea35c398bd47cf231eebf63c350bf4efb3
William Sang <sang.williams@gmail.com>
2014-11-10 15:09:05 +0800

    程序修改正确

--- test_foo.py
+++ test_foo.py
@@ -18,7 +18,7 @@ class SimpleTest(unittest.TestCase):
         :returns: TODO
 
         """
-        self.assertEqual(foo.divide(0,2),1)
+        self.assertEqual(foo.divide(0,2),0)
 
 if __name__ == "__main__":
     unittest.main()
